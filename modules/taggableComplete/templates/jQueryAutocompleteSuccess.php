b04677079a88f5229b7ccf51300f2f95691cbaea
Matt Lehner <matt.lehner@coachmeplus.com>
2022-12-28 23:08:40 -0500

    remove un-used autocomplete widget and module

--- modules/taggableComplete/templates/jQueryAutocompleteSuccess.php
+++
@@ -1,5 +0,0 @@
-<?php $tags = array() ?>
-<?php foreach ($sf_data->getRaw('tagSuggestions') as $tagInfo): ?>
-  <?php $tags[] = $tagInfo['suggested'] ?>
-<?php endforeach ?>
-<?php echo json_encode($tags) ?>
