7048a30089c9e2b6de79ce5d0526de0cbc1a93b0
Matt Lehner <matt.lehner@coachmeplus.com>
2023-01-05 10:14:52 -0500

    remove unused TagsHelper, and unused routing

--- config/sfDoctrineActAsTaggablePluginConfiguration.class.php
+++ config/sfDoctrineActAsTaggablePluginConfiguration.class.php
@@ -2,7 +2,7 @@
 
 /**
  * sfDoctrineActAsTaggablePluginConfiguration configuration.
- * 
+ *
  * @package     sfDoctrineActAsTaggablePlugin
  * @subpackage  config
  * @author      Your name here
@@ -10,24 +10,4 @@
  */
 class sfDoctrineActAsTaggablePluginConfiguration extends sfPluginConfiguration
 {
-  static $registered = false;
-
-  /**
-   * @see sfPluginConfiguration
-   */
-  public function initialize()
-  {
-    // Yes, this can get called twice. This is Fabien's workaround:
-    // http://trac.symfony-project.org/ticket/8026
-    if (!self::$registered)
-    {
-      if (sfConfig::get('app_taggable_routes_register', true))
-      {
-        $this->dispatcher->connect('routing.load_configuration', array('taggableRouting', 'listenToRoutingLoadConfigurationEvent'));
-      }
-
-      self::$registered = true;
-    }
-  }
-  
 }
